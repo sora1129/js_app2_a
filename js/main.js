3261e60da6fb4e742250411af3ade1fd5e6a2a11
sora1129 <yuka.izumisawa@sweeep.ai>
2022-09-08 17:10:38 +0900

    create main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,43 @@
 'use strict'
 
 {
+  function createColumn(col) {
+    // 1~15の配列を作る（ランダム）
+    const source = [];
+    for (let i = 0; i < 15; i++) {
+      source[i] = i + 1 + 15 * col;
+    }
+    
+    const column = [];
+    for (let i = 0; i < 5; i++) {
+      column[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    }
+    return column;
+  }
+
+  function createColumns() {
+    const columns = [];
+    for (let i = 0; i < 5; i++) {
+      columns[i] = createColumn(i);
+    }
+    columns[2][2] = 'FREE';
+    return columns;
+  }
+  
+  function renderBingo(columns) {
+    // tr,td要素を作ってtbodyに入れる
+    for (let row = 0; row < 5; row++) {
+      const tr = document.createElement('tr');
+      for (let col = 0; col < 5; col++) {
+        const td = document.createElement('td');
+        td.textContent = columns[col][row];
+        tr.appendChild(td);
+      }
+      document.querySelector('tbody').appendChild(tr);
+    }
+  }
+
+  const columns = createColumns();
+  renderBingo(columns);
 
 }
