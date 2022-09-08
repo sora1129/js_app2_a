05278c98b382c320b4ed09c948bb57e049d73318
sora1129 <yuka.izumisawa@sweeep.ai>
2022-09-08 17:19:52 +0900

    fete: 数値の基準を作る

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 'use strict'
 
 {
+  function createColumn(col) {
+    // 1～15 * iのランダムの配列を作成し、5個繰り返す
+    const source = [];
+    for (let i = 0; i < 15; i++) {
+      source[i] = i + 1 + 15 * col;
+    }
   
+    const column = [];
+    for (let i = 0; i < 5; i++) {
+      column[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    }
+    return column;
+  }
+
+  // 上の数字を5列分つくる
+  const columns = [];
+  for (let i = 0; i < 5; i++) {
+    
+  }
 
 }
